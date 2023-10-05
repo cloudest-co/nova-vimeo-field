21e0dc50715ead3238a9d02d93bd367bb187bbab
Chris Reid <chrisreiduk@gmail.com>
2023-10-05 13:46:48 +0100

    First commit

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,37 @@
+<template>
+  <PanelItem :field="field">
+    <template v-slot:value>
+      <iframe
+          v-if="field.value"
+          ref="player"
+          :src="videoUrl"
+          :height="height"
+          :width="width"
+          frameborder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen
+      >
+      </iframe>
+      <div v-else>No URL entered</div>
+    </template>
+  </PanelItem>
+</template>
+
+<script>
+export default {
+  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+
+  computed: {
+    videoUrl() {
+      return this.field.value;
+    },
+    height() {
+      return this.field.options.height;
+    },
+    width() {
+      return this.field.options.width;
+    },
+  },
+}
+</script>
